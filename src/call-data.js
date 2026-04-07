9f046d036c012796cb66431c01ac7d280c8b76d9
Gaed <oger.gaedic@gmail.com>
2026-04-07 15:04:37 +0200

    config api

--- src/call-data.js
+++ src/call-data.js
@@ -1,1 +1,15 @@
 //APPEL API et Fetch
+/**
+ */
+export async function callOpenTreeAPI() {
+  try {
+    const response = await fetch(
+      `https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_patrimoine-arbore-nantes-metropole/records?where=annee_plantation%20IS%20NOT%20NULL%20and%20lib_genre%20IS%20%20NOT%20NULL&limit=20&refine=nom%3A%22NANTES%22`,
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import "./style.css";
-
+import { callOpenTreeAPI } from "./call-data";
 //Fichier général pou gérer les fonctions principales
+
+await callOpenTreeAPI();
