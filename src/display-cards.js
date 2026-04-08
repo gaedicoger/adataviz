c19aa9d076ceb42fc68cc376f2b7ee97471ddfad
Gaed <oger.gaedic@gmail.com>
2026-04-08 11:57:44 +0200

    Style des card base

--- src/display-cards.js
+++ src/display-cards.js
@@ -18,13 +18,16 @@ export const displayCards = (tree) => {
   const height = tree.lib_hauteur_totale;
 
   // On crée la div html
-  const cardTree = `
+  const cardTree = `<div class="card-style" >
         <h2 class="tree-card-name">${name}</h2>
         <h3 class="tree-card-subname">${subName}</h3>
-        <p class="tree-card-age">${age}</p>
-        <p class="tree-card-year-plantation">${yearPlantation}</p>
-        <p class="tree-card-height">${height}</p>
-        <p class="tree-card-grow-stade">${grow}</p>`;
+        <p class="tree-card-year-plantation">Année de plantation : ${yearPlantation}</p>
+        <p class="tree-card-age">Age : ${age}</p>
+        <p class="tree-card-height">Taille : ${height}</p>
+        <p class="tree-card-grow-stade">${grow}</p>
+        <button class="see-more"> En savoir +</button>
+        </div>`;
+
   // On l'ajoute au html (la liste)
   list.insertAdjacentHTML("beforeend", cardTree);
 };
