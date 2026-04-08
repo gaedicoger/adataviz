874f992ba9919b8abf36448a7d1bd2d04e7e634a
Gaed <oger.gaedic@gmail.com>
2026-04-08 10:33:36 +0200

    Affichage des infos pour chaque arbres

--- src/call-data.js
+++ src/call-data.js
@@ -1,16 +1,19 @@
 //APPEL API et Fetch
+
+import { displayCards } from "./display-cards";
+
 //Ici récupérer l'ensemble des arbres avec certaines conditions
 export async function callOpenTreeAPI() {
   try {
-    const response = await fetch(
-      `https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_patrimoine-arbore-nantes-metropole/records?where=annee_plantation%20IS%20NOT%20NULL%20and%20lib_genre%20IS%20%20NOT%20NULL&limit=20&refine=nom%3A%22NANTES%22`,
-    );
+    const url = `https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_patrimoine-arbore-nantes-metropole/records?where=annee_plantation%20IS%20NOT%20NULL%20and%20lib_genre%20IS%20%20NOT%20NULL&limit=20&refine=nom%3A%22NANTES%22`;
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    return data;
-  } catch (err) {
-    console.error(err);
+    const results = data.results;
+    results.forEach((tree) => {
+      displayCards(tree);
+    });
+  } catch (error) {
+    console.error("Erreur :", error);
   }
 }
-
-//récupérer les arbres 1 par 1:
--- src/display-cards.js
+++ src/display-cards.js
@@ -1,12 +1,32 @@
 //Gestion de l'affichage des cartes
 //A intégrer dans une fonction
-const cardTree = `
-        <img src="${img}" class="pokemon-card-image" />
-        <h2 class="tree-card-title">${name}</h2>
-        <p class="tree-card-species">${typesString}</p>
-        <p class="tree-card--under-species">${typesString}</p>
-        <p class="tree-card-year_plantation">${typesString}</p>
-        <p class="tree-card-age">${number}</p>
-        <p class="tree-card-height">${number}</p>
-        <p class="tree-card-grow-stade">${img}</p>`;
+
+//Récupèrer la grid :
+const list = document.getElementById("cards-content");
+
+export const displayCards = (tree) => {
+  // On récupère les noms d'espèces
+  const name = tree.lib_espece;
+  const subName = tree.lib_genre;
+  // On récupère l'age
+  const age = tree.classe_age;
+  //On récupère l'année de plantation
+  const yearPlantation = tree.annee_plantation;
+  // On récupère le stade de dev
+  const grow = tree.lib_stade_developpement;
+  //On récupère la taille
+  const height = tree.lib_hauteur_totale;
+
+  // On crée la div html
+  const cardTree = `
+        <h2 class="tree-card-name">${name}</h2>
+        <h3 class="tree-card-subname">${subName}</h3>
+        <p class="tree-card-age">${age}</p>
+        <p class="tree-card-year-plantation">${yearPlantation}</p>
+        <p class="tree-card-height">${height}</p>
+        <p class="tree-card-grow-stade">${grow}</p>`;
+  // On l'ajoute au html (la liste)
+  list.insertAdjacentHTML("beforeend", cardTree);
+};
+
 //Gestion du bouton "See more" pour afficher la description
