eae856b1a4fe4a62b98b614b2f700fecabf11489
Gaed <oger.gaedic@gmail.com>
2026-04-16 12:26:45 +0200

    deploiement

--- api-fonction.test.js
+++ api-fonction.test.js
@@ -1,13 +1,13 @@
-import { describe, it, expect } from "vitest";
-import { callOpenTreeAPI } from "./src/call-data";
+// import { describe, it, expect } from "vitest";
+// import { callOpenTreeAPI } from "./src/call-data";
 
-describe("Appel de l'API", () => {
-  it("Vérifier que la fonction a été appelée", () => {
-    // Créer une fonction mock (virtualiser l'appel de l'API)
-    const mockFn = vi.fn();
-    // Appeler la fonction mock
-    mockFn("arg1", "arg2");
-    //Vérifier que la fonction a été appelée
-    expect(mockFn).callOpenTreeAPI();
-  });
-});
+// describe("Appel de l'API", () => {
+//   it("Vérifier que la fonction a été appelée", () => {
+//     // Créer une fonction mock (virtualiser l'appel de l'API)
+//     const mockFn = vi.fn();
+//     // Appeler la fonction mock
+//     mockFn("arg1", "arg2");
+//     //Vérifier que la fonction a été appelée
+//     expect(mockFn).callOpenTreeAPI();
+//   });
+// });
